b955948fb26fcd673553debd5190dd826f169ff1
Stéphane Klein <contact@stephane-klein.info>
2024-01-01 22:01:01 +0100

    WIP

--- scripts/generate_dummy_rows.sh
+++ scripts/generate_dummy_rows.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(dirname "$0")/../"
 
 export PGPASSWORD=password
-psql -n -q -d postgres -h localhost -p 5432 -U postgres -c "select insert_dummy_records(10);"
+psql -n -q -d postgres -h localhost -p 5432 -U postgres -c "select insert_dummy_records(10); select * from dummy order by id desc limit 1"
